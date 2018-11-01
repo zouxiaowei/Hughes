b276eae968f8a0fadd6fa2d821eed9cd03efe5f0
zouxiaowei <1191745123@qq.com>
2018-11-01 15:08:14 +0800

    grow script

--- Assets/Scripts/Grow.cs
+++ Assets/Scripts/Grow.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 
 public class Grow : MonoBehaviour {
 	bool isGrowing = false;
+	Vector3 initPosition;
+	Vector3 endPosition;
 	// Use this for initialization
 	void Start () {
 		
+		initPosition = this.transform.position;
+		endPosition = new Vector3 (initPosition.x, initPosition, y, initPosition.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (isGrowing && transform.position.y < endPosition.y) {
+			
+			this.gameObject.transform.Translate (new Vector3 (0, 0.01, 0));
+
+		} else if (isGrowing == false && transform.position.y > initPosition.y) {
+			
+			this.gameObject.transform.Translate (new Vector3 (0, -0.01, 0));
+		} else {
+
+			return;
+		}
+
 	}
 
 	public void growUp(){
 		GlobalData data = GlobalData.Instance;
 		if(data.growingPlant.Count >= data.MaxPlantNum){
+			
 			GameObject diePlant = data.growingPlant.Dequeue();
 			data.growingPlant.Enqueue(this.gameObject);
 			diePlant.GetComponent<Grow>().dieDown();
 		}
+		this.isGrowing = true;
 
-		Vector3 position =  this.gameObject.transform.position;
-		position.y += 2;
-		this.gameObject.transform.Translate(new Vector3(0,2,0));
 	}
 
 	public void dieDown(){
-		this.gameObject.transform.Translate(new Vector3(0,-2,0));
+		this.isGrowing = false;
 	}
 
 }
