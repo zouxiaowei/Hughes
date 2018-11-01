a1a79f6f032415d9b3f24ed54b929d17ca0f03ee
zouxiaowei <1191745123@qq.com>
2018-11-01 17:41:57 +0800

    1

--- Assets/Scripts/WorldTypeControl.cs
+++ Assets/Scripts/WorldTypeControl.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class WorldTypeControl : MonoBehaviour {
 	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	public void ChangeWorldType(){
 		GameObject[] backgrounds = GameObject.FindGameObjectsWithTag("Background");
